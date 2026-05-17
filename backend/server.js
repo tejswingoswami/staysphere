0267bb8e8081cf9963ed90f2480623eb493dedb0
goswamitejswin-pixel <goswamitejswin@gmail.com>
2026-05-17 21:37:33 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,13 @@ import cors from "cors"
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+)
 app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/staysphere")
